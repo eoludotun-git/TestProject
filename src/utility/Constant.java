a31778abe6800c088e10e59f0049b2848abd4f01
eoludotun-git <eoludotun@gmail.com>
2016-05-29 10:05:20 -0400

    Update Constant.java
    good

--- src/utility/Constant.java
+++ src/utility/Constant.java
@@ -2,39 +2,17 @@ package utility;
 
 public class Constant {
 	
-	//public static final String URL = "https://t3prod.cloudapp.net/Account/Login";
+
 	
-	//public static final String URL = "https://t3qa.cloudapp.net/Account/Login";
-	
-	public static final String URL = "https://t3dev.cloudapp.net/Account/Login";
-	
-    //public static final String Username = "eoludotun@troygroup.com";
-	
-    public static final String Username = "Kadabulabambulu@gmail.com";
+	public static final String URL = "https://some URL/Login";
+
     
-   
-    public static final String Password = "Troypass1";
-    
-    //public static final String Username_B = "kadabulabambulu@gmail.com";
-    
-   public static final String Username_B = "sansancobrandowner@gmail.com";
+   public static final String Username_B = "some username";
    
     public static final String Password_B = "Testpass1";
     
     public static final String Password_B_2 = "Testpass2";
-    
-    
-    public static final String Username_L = "labelconventer1@gmail.com";
-    
-    public static final String Password_L = "Testpass1";
-    
-    public static final String Password_L_2 = "Testpass2";
-    
-    
    
-    public static final String Path_TestData = "C://TESTER FRAME WORK//Track and Trace Project//src//testData";
-    //public static final String Path_TestData = "C:\\Users\\Ebenezer\\Desktop\\src\\testData\\";
-   public static final String File_TestData = "TestData.xlsx";
 
   
    
